969ed159c5bc204db2ea4b73e9f492c0cc51e2ae
aiiikmrtuz_ <83161765+aiiikmrtuz@users.noreply.github.com>
2021-10-31 22:00:01 +0700

    banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 
-    <title>Pet Care</title>
+    <title>petcare</title>
 
     <!-- Bootstrap core CSS -->
     <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -54,7 +54,7 @@ https://templatemo.com/tm-563-seo-dream
           <nav class="main-nav">
             <!-- ***** Logo Start ***** -->
             <a href="#" class="logo">
-              <h4>SEO Dream <img src="assets/images/logo-icon.png" alt=""></h4>
+              <h4>PET CARE </h4>
             </a>
             <!-- ***** Logo End ***** -->
             <!-- ***** Menu Start ***** -->
@@ -86,30 +86,12 @@ https://templatemo.com/tm-563-seo-dream
             <div class="col-lg-6 align-self-center">
               <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
                 <div class="row">
-                  <div class="col-lg-4 col-sm-4">
-                    <div class="info-stat">
-                      <h6>Agency Status:</h6>
-                      <h4>Ready Work</h4>
-                    </div>
-                  </div>
-                  <div class="col-lg-4 col-sm-4">
-                    <div class="info-stat">
-                      <h6>Price:</h6>
-                      <h4>$720/Month</h4>
-                    </div>
-                  </div>
-                  <div class="col-lg-4 col-sm-4">
-                    <div class="info-stat">
-                      <h6>Schedules</h6>
-                      <h4>$450/Meeting</h4>
-                    </div>
+                  <div class="col-lg-12">
+                    <h2>Your Digital <br> Pet Assistant </h2>
                   </div>
                   <div class="col-lg-12">
-                    <h2>SEO &amp; Digital Marketing Agency</h2>
-                  </div>
-                  <div class="col-lg-12">
-                    <div class="main-green-button scroll-to-section">
-                      <a href="#contact">Get Your Quote</a>
+                    <div>
+                      <h2></h2>
                     </div>
                   </div>
                 </div>
@@ -117,7 +99,7 @@ https://templatemo.com/tm-563-seo-dream
             </div>
             <div class="col-lg-6">
               <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                <img src="assets/images/banner-right-image.png" alt="">
+                <img src="assets/images/banner-right-image.jpg" alt="">
               </div>
             </div>
           </div>
